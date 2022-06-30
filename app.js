136dbcaf8cfd61dfd5e21473bcc75e21481e4d1d
mdxaasil <88720914+mdxaasil@users.noreply.github.com>
2022-06-30 23:21:29 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -86,7 +86,53 @@ const songs = [
 Array.from(document.getElementsByClassName('songItem')).forEach((element, i)=>{
     element.getElementsByTagName('img')[0].src = songs[i].poster;
     element.getElementsByTagName('h5')[0].innerHTML = songs[i].songName;
+});
+
+// search data start
+
+let search_result = document.getElementsByClassName('search_result')[0];
+
+songs.forEach(element => {
+    const { id, songName , poster} = element;
+    let card = document.createElement('a');
+    card.classList.add('card');
+    card.href = '#' + id;
+    card.innerHTML = `
+    <img src="${poster}" alt="">
+    <div class="content">
+        ${songName}
+    </div>
+    `;
+    search_result.appendChild(card);
+});
+
+let input = document.getElementsByTagName('input')[0];
+
+input.addEventListener('keyup', ()=>{
+    let input_value = input.value.toUpperCase();
+    let items = search_result.getElementsByTagName('a');
+
+    for (let i=0; i< items.length; i++){
+        let as = items[i].getElementsByClassName('content')[0];
+        let text_value = as.textContent || as.innerText;
+
+        if(text_value.toUpperCase(). indexOf(input_value) > -1) {
+           items[i].style.display ="flex"; 
+        }else{
+           items[i].style.display = "none";
+        }
+
+        if (input.value == 0) {
+            search_result.style.display = "none";
+        }else{
+            search_result.style.display = "";
+        }
+
+    }
 })
+
+// search data end
+
 
 
 let masterPlay = document.getElementById('masterPlay');
--- faq.js
+++ faq.js
@@ -1,15 +1,76 @@
-const items = document.querySelectorAll('.faq button');
+//drop-down
+const items = document.querySelectorAll(".faq button");
 
 function togglefaq() {
-  const itemToggle = this.getAttribute('aria-expanded');
+  const itemToggle = this.getAttribute("aria-expanded");
 
   for (i = 0; i < items.length; i++) {
-    items[i].setAttribute('aria-expanded', 'false');
+    items[i].setAttribute("aria-expanded", "false");
   }
 
-  if (itemToggle == 'false') {
-    this.setAttribute('aria-expanded', 'true');
+  if (itemToggle == "false") {
+    this.setAttribute("aria-expanded", "true");
   }
 }
 
-items.forEach((item) => item.addEventListener('click', togglefaq));
+//form
+
+items.forEach((item) => item.addEventListener("click", togglefaq));
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+function checkInputs() {
+  // trim to remove the whitespaces
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+  if (usernameValue === "") {
+    setErrorFor(username, "Username cannot be blank");
+  } else {
+    setSuccessFor(username);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(email, "Email cannot be blank");
+  } else {
+    setSuccessFor(email);
+  }
+
+  if (passwordValue === "") {
+    setErrorFor(password, "Password cannot be blank");
+  } else {
+    setSuccessFor(password);
+  }
+
+  if (password2Value === "") {
+    setErrorFor(password2, "Password2 cannot be blank");
+  } else if (passwordValue !== password2Value) {
+    setErrorFor(password2, "Passwords does not match");
+  } else {
+    setSuccessFor(password2);
+  }
+}
+
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+  formControl.className = "form-control error";
+  small.innerText = message;
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
